0d7e510909470f168ca5ad91f763809d71a63258
John Padilla <johnpadi8790@gmail.com>
2022-03-01 00:50:42 -0700

    DataPersistenceManager

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -63,7 +63,15 @@ class CollectionViewTableViewCell: UITableViewCell {
     }
     
     private func downloadTitleAt(indexPath: IndexPath) {
-        print("Downloading \(title[indexPath.row].original_title)")
+        
+        DataPersistenceManager.shared.downloadTitleWithModal(model: title[indexPath.row]) { result in
+            switch result {
+            case .success():
+                print("Downloading to Database")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
