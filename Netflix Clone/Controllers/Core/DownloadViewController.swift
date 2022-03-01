4c6ebe7412e20e4f6068a1f777dc9686617f217d
John Padilla <johnpadi8790@gmail.com>
2022-03-01 01:08:14 -0700

    DownloadController
    Create TableView for Donwload Controller

--- Netflix Clone/Controllers/Core/DownloadViewController.swift
+++ Netflix Clone/Controllers/Core/DownloadViewController.swift
@@ -7,16 +7,66 @@
 
 import UIKit
 
+private let identifier = "TitleTableViewCell"
+
 class DownloadViewController: UIViewController {
     
     //MARK: - Properties
     
+    
+    private let DownloadedTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: identifier)
+        return table
+    }()
     
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        configureNavBar()
+        configureTableView()
     }
+    
+    //MARK: - Helpers
+    
+    func configureNavBar(){
+        view.backgroundColor = .systemBackground
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    func configureTableView() {
+        view.addSubview(DownloadedTable)
+        DownloadedTable.delegate = self
+        DownloadedTable.dataSource = self
+    }
+}
+
+//MARK: - UITableViewDataSource
+
+extension DownloadViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}
+
+//MARK: - UITableViewDelegate
+
+extension DownloadViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 155
+    }
+    
 }
